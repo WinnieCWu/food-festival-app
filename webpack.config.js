08c8f4b937b5d1a218b2f8b6d7f3244da49b65bb
WinnieCWu <winniechn7@gmail.com>
2022-05-02 09:19:20 -0700

    installed webpack loaders to optimize images and added multiple entry points

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,45 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 const path = require("path");
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/event.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-        },
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist"
+    },
+    module: {
+        rules: [
+            //this obj in the rules array will ID the type of files to pre-process
+            //using the test property to find a regex (with .jpg ext)
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      //options enable us to rename our files and change output path
+                      options: {
+                          //file-loader treats file as ES5 module and may improperly format imgs
+                          //setting it to false would prevent improper formatting 
+                        esModule: false,
+                        name (file) {
+                          return "[path][name].[ext]"
+                        },
+                        publicPath: function(url) {
+                          return url.replace("../", "/assets/")
+                        }
+                      }  
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         //tell webpack which plugin we what to use
         //providePlugin plugin defines the $ and jQuery variables to use the installed npm package
@@ -20,5 +54,6 @@ module.exports = {
                 //can set to 'disable' to temporarily stop the reporting and automatic opening of this report in browser
         })
     ],
-    mode: 'development'
+    mode: "development"
 };
+
